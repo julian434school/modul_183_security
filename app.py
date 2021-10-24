f3a07dd69271b9fc50db08dd2c8833a2d6c8526b
Julian443 <Lerois88>
2021-10-24 23:13:29 +0200

    changed localhost port

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
 
 from REST.weather_service import *
 
@@ -11,4 +11,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host='0.0.0.0')
