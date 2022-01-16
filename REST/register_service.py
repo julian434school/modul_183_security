857a03ce1cd3eb355fbe0a3a7b27f1a434712715
namikoya <nati.wernli@gmail.com>
2022-01-16 14:03:53 +0100

    added html patterns

--- REST/register_service.py
+++ REST/register_service.py
@@ -2,13 +2,26 @@ import cgi
 
 import bcrypt
 
+import re
+
 from db.register_db_controller import insert_into_database
 
 
 def save_data_to_database(username, email, password):
-    hash_and_salt = hash_password_save_salt(password)
-    insert_into_database(username, email, hash_and_salt.get("hashed_password").decode('utf-8'),
-                         hash_and_salt.get("salt").decode('utf-8'))
+    if isValid(username.strip(), email.strip(), password.strip()):
+        hash_and_salt = hash_password_save_salt(password)
+        insert_into_database(username, email, hash_and_salt.get("hashed_password").decode('utf-8'),
+                             hash_and_salt.get("salt").decode('utf-8'))
+    else:
+        # TODO: error message, how to show user?
+        e = email
+
+
+def isValid(username: str, email: str, password: str):
+    isValidUsername = re.search("^[a-zA-Z][a-zA-Z0-9-_.]{4,20}$", username)
+    isValidEmail = re.search("^[a-zA-Z][a-zA-Z0-9-_.]{4,20}$", email)
+    isValidPassword = re.search("^[a-zA-Z][a-zA-Z0-9-_.]{4,20}$", password)
+    return isValidUsername and isValidEmail and isValidPassword
 
 
 def hash_password_save_salt(password: str):
